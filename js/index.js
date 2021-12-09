4fe0fa7c7cca835376d69a6d4804f51a6b20423a
Jayallstars <jayza0072555@gmail.com>
2021-12-09 20:20:19 +0700

    Create a simple random dice using DOM.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var randomNumber1 = Math.ceil(Math.random() * 6);
+var randomNumber2 = Math.ceil(Math.random() * 6);
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+} else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
